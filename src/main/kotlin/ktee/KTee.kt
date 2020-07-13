6fff5623c8655558ba49f8f2838492796f2b1cc0
Aristos Pasalides <apasalides@axiomc.com>
2020-07-13 13:47:43 +0300

    Refactored extension functions.

--- src/main/kotlin/ktee/KTee.kt
+++ src/main/kotlin/ktee/KTee.kt
@@ -14,71 +14,53 @@ class KTee { companion object { var debug = true } }
  * myList.map(fn).tee(">>> ").reduce(fn)
  *
  */
-fun <T> T.tee(marker: String = ""): T {
-    if (debug) println(marker + this)
-    return this
-}
+fun <T> T.tee(marker: String = "") = apply { if (debug) println(marker + this) }
 
 /**
  *
  * executes the lambda with the value of the chain and writes the
  *
  */
-inline fun <T> T.tee(fn: (T) -> String): T {
-    if (debug) println(fn(this))
-    return this
-}
+inline fun <T> T.tee(fn: (T) -> String) = apply { if (debug) println(fn(this)) }
 
 /**
  * logs the value to the given logger at info level. Message can be customized using message parameter
  */
-fun <T> T.teeToInfo(logger: Logger, message: String = "{}"): T {
-    if (debug) logger.info(message, this)
-    return this
-}
+fun <T> T.teeToInfo(logger: Logger, message: String = "{}")
+        = apply { if (debug) logger.info(message, this) }
 
 /**
  * Evaluates the lambda and logs the result (of evaluation) to the given logger at info level
  *
  */
-inline fun <T> T.teeToInfo(logger: Logger, fn: (T) -> String): T {
-    if (debug) logger.info(fn(this), this)
-    return this
-}
+inline fun <T> T.teeToInfo(logger: Logger, fn: (T) -> String)
+        = apply { if (debug) logger.info(fn(this), this) }
 
 /**
  * logs the value to the given logger at info level. Message can be customized using message parameter
  */
-fun <T> T.teeToDebug(logger: Logger, message: String = "{}"): T {
-    if (debug) logger.debug(message, this)
-    return this
-}
+fun <T> T.teeToDebug(logger: Logger, message: String = "{}")
+        = apply { if (debug) logger.debug(message, this) }
 
 /**
  * Evaluates the lambda and logs the result (of evaluation) to the given logger at debug level
  *
  */
-inline fun <T> T.teeToDebug(logger: Logger, fn: (T) -> String): T {
-    if (debug) logger.debug(fn(this), this)
-    return this
-}
+inline fun <T> T.teeToDebug(logger: Logger, fn: (T) -> String)
+        = apply { if (debug) logger.debug(fn(this), this) }
 
 /**
  * logs the value to the given logger at trace level. Message can be customized using message parameter
  */
-fun <T> T.teeToTrace(logger: Logger, message: String = "{}"): T {
-    if (debug) logger.trace(message, this)
-    return this
-}
+fun <T> T.teeToTrace(logger: Logger, message: String = "{}")
+        = apply { if (debug) logger.trace(message, this) }
 
 /**
  * Evaluates the lambda and logs the result (of evaluation) to the given logger at trace level
  *
  */
-inline fun <T> T.teeToTrace(logger: Logger, fn: (T) -> String): T {
-    if (debug) logger.trace(fn(this), this)
-    return this
-}
+inline fun <T> T.teeToTrace(logger: Logger, fn: (T) -> String)
+        = apply { if (debug) logger.trace(fn(this), this) }
 
 
 
