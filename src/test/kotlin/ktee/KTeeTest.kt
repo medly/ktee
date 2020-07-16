14a1d472d7a6e23ebb6aab057d75822bf60164a1
Aristos Pasalides <apasalides@axiomc.com>
2020-07-16 23:53:47 +0300

    Created tests for when debug is false.

--- src/test/kotlin/ktee/KTeeTest.kt
+++ src/test/kotlin/ktee/KTeeTest.kt
@@ -4,6 +4,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
 
 class KTeeTest {
@@ -64,4 +65,60 @@ class KTeeTest {
         outputs.forEach { assertTrue { it.endsWith("hello myval\n") } }
     }
 
+    @Test
+    fun `should throw IllegalStateException`() {
+        assertFailsWith<IllegalStateException> {
+            KTee.debug = false; KTee.debug = false }
+    }
+
+    @Test
+    fun `should not write to stdout`() {
+        if (!KTee.debugChanged) KTee.debug = false
+        assertEquals("", trapOut { "myval".tee() })
+    }
+
+    @Test
+    fun `should evaluate lambda and not write to stdout`() {
+        if (!KTee.debugChanged) KTee.debug = false
+        assertEquals("", trapOut { "myval".tee { v -> "value is $v" } })
+    }
+
+    @Test
+    fun `should not write to logger`() {
+        if (!KTee.debugChanged) KTee.debug = false
+        assertTrue(trapErr { "myval".teeToInfo(logger) } == "")
+    }
+
+    @Test
+    fun `should not log with info level`() {
+        if (!KTee.debugChanged) KTee.debug = false
+        val outputs = listOf(
+                trapErr { "myval".teeToInfo(logger, "hello {}") },
+                trapErr { "myval".teeToInfo(logger) { "hello $it" } },
+                trapErr { "myval".teeToInfo(logger) { "hello {}" } }
+        )
+        outputs.forEach { assertTrue { !it.contains("INFO") } }
+    }
+
+    @Test
+    fun `should not log with trace level`() {
+        if (!KTee.debugChanged) KTee.debug = false
+        val outputs = listOf(
+                trapErr { "myval".teeToTrace(logger, "hello {}") },
+                trapErr { "myval".teeToTrace(logger) { "hello $it" } },
+                trapErr { "myval".teeToTrace(logger) { "hello {}" } }
+        )
+        outputs.forEach { assertTrue { it == "" } }
+    }
+
+    @Test
+    fun `should not log with debug level`() {
+        if (!KTee.debugChanged) KTee.debug = false
+        val outputs = listOf(
+                trapErr { "myval".teeToDebug(logger, "hello {}") },
+                trapErr { "myval".teeToDebug(logger) { "hello $it" } },
+                trapErr { "myval".teeToDebug(logger) { "hello {}" } }
+        )
+        outputs.forEach { assertTrue { !it.contains("DEBUG") } }
+    }
 }
